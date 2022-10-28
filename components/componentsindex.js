06df19289774903a9041875650ce324311b34a0f
heysourin <sourin07@gmail.com>
2022-10-28 22:52:00 +0530

    Updated

--- components/componentsindex.js
+++ components/componentsindex.js
@@ -4,4 +4,20 @@ import Navbar from "./Navbar/Navbar";
 import HeroSection from "./HeroSection/HeroSection";
 import Service from "./Service/Service";
 import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
-export { Button, Navbar, Footer, HeroSection,Service, BigNFTSlider };
+import Subscribe from "./Subscribe/Subscribe";
+import Title from "./Title/Title";
+import Category from "./Category/Category";
+import Filter from "./Filter/Filter";
+
+export {
+  Button,
+  Navbar,
+  Footer,
+  HeroSection,
+  Service,
+  BigNFTSlider,
+  Subscribe,
+  Title,
+  Category,
+  Filter
+};
--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,28 @@ import {
   HeroSection,
   Service,
   BigNFTSlider,
+  Subscribe,
+  Title,
+  Category,
+  Filter,
 } from "../components/componentsindex";
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
       <Service />
-      <BigNFTSlider/>
+      <BigNFTSlider />
+      <Title
+        heading="Featured NFTs"
+        paragraph="Discover the all outstanding NFTs accross all categories"
+      />
+        <Filter />
+      <Title
+        heading="Browse By Category"
+        paragraph="Explore the NFTs in most featured categories"
+      />
+      <Category />
+      <Subscribe />
     </div>
   );
 };
